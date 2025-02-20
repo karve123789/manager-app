3eb05a2be61511c66310c2600bf93bfcdd3e9774
Карпиза Владимир <karve123789@gmail.com>
2025-02-20 15:49:20 +0300

    README Добавление валидации в ProductPayload

--- src/main/java/ru/umka/manager_app/controller/payload/NewProductPayload.java
+++ src/main/java/ru/umka/manager_app/controller/payload/NewProductPayload.java
@@ -1,5 +1,14 @@
 package ru.umka.manager_app.controller.payload;
 
-public record NewProductPayload (String title, String details){
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewProductPayload (
+        @NotNull
+        @Size(min = 3, max = 30)
+        String title,
+
+        @Size(max = 300)
+        String details){ 
 
 }
--- src/main/java/ru/umka/manager_app/controller/payload/UpdateProductPayload.java
+++ src/main/java/ru/umka/manager_app/controller/payload/UpdateProductPayload.java
@@ -1,4 +1,14 @@
 package ru.umka.manager_app.controller.payload;
 
-public record UpdateProductPayload (String title, String details) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProductPayload (
+        @NotNull
+        @Size(min = 3, max = 30)
+        String title,
+
+        @Size(max = 300)
+        String details){
+
 }
