6a7127d89eb1f665663af5a01935d3fdbdde5695
Карпиза Владимир <karve123789@gmail.com>
2025-02-21 18:27:00 +0300

    Local changes before pull

---
+++ katalog-service/src/main/java/com/example/katalogservice/KatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.katalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatalogServiceApplication.class, args);
+    }
+
+}
---
+++ katalog-service/src/test/java/com/example/katalogservice/KatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
